d2587f35a9555e817cc05425c796620b41e9f23d
VasyaKhokhlov <vvkhokhlov@hotmail.com>
2023-08-07 15:48:49 +0300

    124

--- Home/j.java
+++ Home/j.java
@@ -1,141 +1,55 @@
+//Разработать программу, имитирующую поведение коллекции HashSet. В программе содать метод add добавляющий элемент, 
+// метод toString возвращающий строку с элементами множества и метод позволяющий читать элементы по индексу.
+
 package Home;
 
-import java.util.*;
-
-// Задание
-
-// Реализуйте структуру телефонной книги с помощью HashMap.
-// Программа также должна учитывать, что во входной структуре будут повторяющиеся имена
-// с разными телефонами, их необходимо считать, как одного человека с разными телефонами. 
-
-
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class j {
-
-
-    static void sortedPrint(Map<String, ArrayList> map) {
-
-        
-        Set<String> keySet = map.keySet();
-
-       
-        int maxCount = 0;
-        int minCount = 1_000_000;
-        
-        for (var item : map.entrySet()) {
-            if (maxCount < item.getValue().size())
-                maxCount = item.getValue().size();
-            if (minCount > item.getValue().size())
-                minCount = item.getValue().size();
-            
-        }
-      
-        Stack<String> st = new Stack<>();
-        int num = minCount;
-        while (num <= maxCount) {
-            // System.out.println(map);
-            for (var item : map.entrySet()) {
-                if (item.getValue().size() == num) {
-                    st.push(item.getKey());
-                }  
-            }
-            num += 1;
-        }
-        
-        String name;
-        for (int i = 0; i < keySet.size(); i++) {
-            name = st.pop();
-            for (var item : map.entrySet()) {
-                if (name == item.getKey()) {
-                    System.out.printf("%8s: ", item.getKey());
-                    System.out.println(item.getValue());
-                }
-            }
-        }
-        System.out.println();
-    }
-
-    
-
+    private static HashMap<Integer, Object> hMap = new HashMap<>();
+    private static final Object OBJ = new Object();
     public static void main(String[] args) {
-        
-        Map<String, ArrayList> abon = new HashMap<>() {
-            {
-                put("Хэлпинадзе", new ArrayList<Integer>() {
-                    {
-                        add(795034554);
-                        add(234576);
-                        add(791361423);
-                    }
-                });
-                put("Хрюшкин", new ArrayList<Integer>() {
-                    {
-                        add(555464);
-                    }
-                });
-                put("Жуков", new ArrayList<Integer>() {
-                    {
-                        add(234774);
-                        add(238765);
+        addToHMap(0);
+        addToHMap(7);
+        addToHMap(43);
+        addToHMap(13); 
+        addToHMap(44);
+        addToHMap(1);
+        addToHMap(9);
+        addToHMap(38);
+        addToHMap(10);
+        addToHMap(50);
+        System.out.println(isEmpty());
 
-                    }
-                });
-                put("Персиков", new ArrayList<Integer>() {
-                    {
-                        add(654722);
-                        add(141844);
-                        add(224657);
-                        add(951654);
-                    }
-                });
-            }
-        };
+        printConsole();
         System.out.println();
-        
-        System.out.println("Исходные данные: ");
-        sortedPrint(abon);
-
-        Scanner scan = new Scanner(System.in, "cp866");
-        Boolean getOut = false;
-        String st;
-        while (!getOut) {
-            System.out.println("Введите номер действия (1 - добавить абонента, 9 - выйти из программы):");
-            st = scan.nextLine();
-            String name = "";
-            String phString;
-            switch (st) {
-                case "1": {
-                    System.out.println("Введите фамилию абонента:");
-                    name = scan.nextLine();
-                    if (abon.containsKey(name)) {
-                        System.out.println("Такая фамилия уже есть. Больше фантазии!");
-                        System.out.println();
-                        break;
-                    } else {
-                        System.out.println("Введите номера телефонов через запятую: ");
-                        phString = scan.nextLine();
-                        String[] arr = phString.split(",");
-                        ArrayList<Integer> arrInt = new ArrayList<>();
-                        for (String item: arr) {
-                            arrInt.add(Integer.parseInt(item.trim())) ;
-                        }
-                        abon.put(name, arrInt);
-                        System.out.println();
-                        sortedPrint(abon);
-                        break;
-                    }
-                }
-                case "9": {
-                    getOut = true;
-                    System.out.println();
-                    System.out.println("До свидания!");
-                    System.out.println();
-                    break;
-                }   
-                    
-               
-            }
+        try {
+            System.out.println(printOneEl(20));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
+    public static void addToHMap(Integer number){
+        hMap.put(number, OBJ);   
+    } 
+
+
+    private static boolean isEmpty(){
+        return hMap.isEmpty();
+    }
+
+
+    private static void printConsole(){
+        Iterator<Integer> iterator = hMap.keySet().iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+    }
+
+
+    private static int printOneEl(int index){
+        return (Integer)hMap.keySet().toArray()[index];
+    }      
 }
